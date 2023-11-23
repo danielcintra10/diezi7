9f4b5b18e12b5a0e6058fe280557eb8bacb2f8e3
Daniel Cintra <dany.webd3v@gmail.com>
2023-11-23 11:22:11 -0500

    ✨ feat: contact section add
    - contact section define
    - modifiy placeholders
    - error text modified
    - text succes modified
    - add facebook and intagram contact
    - left server configuration

--- mail/contact.js
+++ mail/contact.js
@@ -29,7 +29,7 @@ $(function () {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
+                            .append("<strong>Su mensaje ha sido enviado correctamente. </strong>");
                     $('#success > .alert-success')
                             .append('</div>');
                     $('#contactForm').trigger("reset");
@@ -38,7 +38,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Lo sentimos " + name + ", parece que nuestro servidor de correo no responde. ¡Por favor, inténtelo de nuevo más tarde!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
--- mail/contact.php
+++ mail/contact.php
@@ -9,9 +9,9 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "eldany0510@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$body = "Has recibido un nuevo mensaje desde la página de diezi7oficial. \n\n"."Aquí están los detalles:\n\nNombre: $name\n\n\nCorreo: $email\n\nAsunto: $m_subject\n\nMensaje: $message";
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
